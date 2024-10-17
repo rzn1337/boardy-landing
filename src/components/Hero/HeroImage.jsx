d5ee8d7f824438066380cdf82d66748a0c944664
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-10-17 13:28:48 +0500

    add react lazy

---
+++ src/components/Hero/HeroImage.jsx
@@ -0,0 +1,11 @@
+const HeroImage = () => {
+    return (
+        <img
+            src="/images/heroimage.png"
+            alt="Boardy app in action"
+            className="mx-auto rounded-lg shadow-lg w-full md:w-3/5"
+        />
+    );
+};
+
+export default HeroImage;
